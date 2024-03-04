3961d437be4bce53924a6076fe83785236d827fc
Fabio Guaidia <fabioguaidia@hotmail.com>
2024-03-04 16:22:47 -0500

    Spring boot project basic config

---
+++ src/test/java/dev/fabioguaidia/market/health/HealthControllerTest.java
@@ -0,0 +1,15 @@
+package dev.fabioguaidia.market.health;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+public class HealthControllerTest {
+
+    @InjectMocks
+    private HealthController healthController;
+
+
+}
